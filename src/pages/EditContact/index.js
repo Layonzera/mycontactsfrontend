3c6f329925ebcbd9467e9672405f1464afd4af01
Adryann Layon <adryann.layon7@gmail.com>
2024-01-23 21:57:57 -0300

    :sparkles: feat: using contact form im pages

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,7 +1,11 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar Adryann Layon" />
+    <>
+      <PageHeader title="Editar Adryann Layon" />
+      <ContactForm buttonLabel="Salvar alteracoes" />
+    </>
   );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,24 +1,12 @@
-import Button from '../../components/Button';
-import Input from '../../components/Input';
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
-import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
 
-      <Input placeholder="Nome" />
-      <Select>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-      </Select>
-
-      <Button type="button">
-        Salvar alteracoes
-      </Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
